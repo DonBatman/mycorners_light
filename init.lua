9801d5e27420e83e71ee2fa81b01153eda3c2187
DonBatman <serfdon@gmail.com>
2015-08-24 20:14:35 -0700

    First upload

---
+++ init.lua
@@ -0,0 +1,14 @@
+mycorners = {}
+--dofile(minetest.get_modpath("mycorners_light").."/cornertool.lua")
+dofile(minetest.get_modpath("mycorners_light").."/nodes.lua")
+dofile(minetest.get_modpath("mycorners_light").."/machine.lua")
+dofile(minetest.get_modpath("mycorners_light").."/register.lua")
+--dofile(minetest.get_modpath("mycorners").."/corners.lua")
+
+
+
+
+
+
+
+
---
+++ register.lua
@@ -0,0 +1,108 @@
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_acacia_wood", 
+	"Acacia Wood", 
+	"default_acacia_wood.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:acacia_wood",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_clay", 
+	"Clay", 
+	"default_clay.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:clay",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_cobble", 
+	"Cobble", 
+	"default_cobble.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:cobble",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_stone", 
+	"Stone", 
+	"default_stone.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:stone",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_desert_stone", 
+	"Desert Stone", 
+	"default_desert_stone.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:desert_stone",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, item
+	"default_wood", 
+	"Wood", 
+	"default_wood.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:wood",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_pine_wood", 
+	"Pine Wood", 
+	"default_pine_wood.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:pine_wood",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_desert_cobble", 
+	"Desert Cobble", 
+	"default_desert_cobble.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:desert_cobble",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_junglewood", 
+	"Jungle Wood", 
+	"default_junglewood.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1,not_in_creative_inventory=1}",
+	"default:junglewood",
+	"normal"
+	)
+
+mycorners.register_all(--material, description, image, groups, craft item
+	"default_sandstone", 
+	"Sandstone", 
+	"default_sandstone.png",
+	"{cracky = 1, oddly_breakable_by_hand = 1, not_in_creative_inventory=1}",
+	"default:sandstone",
+	"normal"
+	)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
