4f8bc4f2710c505153e201e1ae78491a49a2b502
DonBatman <serfdon@gmail.com>
2015-08-26 12:59:12 -0700

    Removed machine and most dyes

---
+++ cornertool.lua
@@ -0,0 +1,113 @@
+local USES = 200
+
+local cornerblock = {
+{"wood_brown", 	       "corners_wood_brown.png",          "Brown Wood"},
+{"wood_green",         "corners_wood_green.png",          "Green Wood"},
+{"wood_red", 	       "corners_wood_red.png",            "Red Wood"},
+{"wood_white", 	       "corners_wood_white.png",          "White Wood"},
+{"stone_brown",           "corners_stone_brown.png",          "Brown Stone"},
+{"stone_green",           "corners_stone_green.png",          "Green Stone"},
+{"stone_red", 	          "corners_stone_red.png",            "Red Stone"},
+{"stone_white",           "corners_stone_white.png",          "White Stone"},
+{"stonebrick_brown", 	      "corners_stonebrick_brown.png",          "Brown Stone Brick"},
+{"stonebrick_green",          "corners_stonebrick_green.png",          "Green Stone Brick"},
+{"stonebrick_red", 	      "corners_stonebrick_red.png",            "Red Stone Brick"},
+{"stonebrick_white", 	      "corners_stonebrick_white.png",          "White Stone Brick"},
+
+}
+
+local default_material = {
+		{"default:cobble", "default_cobble"},
+		{"default:sandstone","default_sandstone"},
+		{"default:clay","default_clay"},
+		{"default:stone","default_stone"},
+		{"default:desert_stone","default_desert_stone"},
+		{"default:wood","default_wood"},
+		{"default:acacia_wood","default_acacia_wood"},
+		{"default:pine_wood","default_pine_wood"},
+		{"default:brick","default_brick"},
+		{"default:desert_cobble","default_desert_cobble"},
+		{"default:junglewood","default_junglewood"},
+		{"default:mossycobble","default_mossycobble"},
+		{"default:sandstonebrick","default_sandstone_brick"},
+		{"default:stonebrick","default_stone_brick"},
+		{"default:desert_stonebrick","default_desert_stone_brick"},
+		}
+
+minetest.register_tool( "mycorners_light:corner_tool",{
+	description = "Corner Tool",
+	inventory_image = "mycorners_cornertool.png",
+	wield_image = "mycorners_cornertool.png",
+	on_use = function(itemstack, user, pointed_thing)
+	if pointed_thing.type ~= "node" then
+		return
+	end
+for i in ipairs(cornerblock) do
+	local mat = cornerblock[i][1]
+	local img = cornerblock[i][2]
+	local desc = cornerblock[i][3]
+
+for i in ipairs(default_material) do
+	local material = default_material[i][1]
+	local iname = default_material[i][2]
+	local beside = user:get_wield_index()+1
+	local inv = user:get_inventory()
+  	local istack = inv:get_stack("main", beside)
+	local pos = pointed_thing.under
+	local node = minetest.get_node(pos)
+	local cornr = "mycorners_light:corner_"..mat
+
+	if node.name == material and
+           istack:get_name() == cornr
+	then
+		minetest.set_node(pos,{name = "mycorners_light:cornerblock_"..iname.."_"..mat, param2=minetest.dir_to_facedir(user:get_look_dir())})
+        	user:get_inventory():remove_item("main", cornr.." 1");
+	break
+	end
+end
+end
+
+
+	if not minetest.setting_getbool("creative_mode") then
+		itemstack:add_wear(65535 / (USES - 1))
+	end
+	return itemstack
+	
+
+end,
+
+	on_place = function(itemstack, placer, pointed_thing)
+	local pos = pointed_thing.under
+	local node = minetest.get_node(pos).name
+	local para = minetest.get_node(pos).param2
+	local newpara = para + 1
+		if newpara > 3 then
+		newpara = 0
+		end
+	minetest.set_node(pos,{name = ""..node, param2 = newpara})
+
+	if not minetest.setting_getbool("creative_mode") then
+		itemstack:add_wear(65535 / (USES - 1))
+	end
+	return itemstack
+	end,
+})
+minetest.register_craft({
+		output = 'mycorners_light:corner_tool',
+		recipe = {
+			{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+			{'', 'default:steel_ingot', ''},
+			{'', "wool:blue", ''},		
+		},
+})
+
+
+
+
+
+
+
+
+
+
+
--- init.lua
+++ init.lua
@@ -1,9 +1,120 @@
-mycorners = {}
---dofile(minetest.get_modpath("mycorners_light").."/cornertool.lua")
-dofile(minetest.get_modpath("mycorners_light").."/nodes.lua")
-dofile(minetest.get_modpath("mycorners_light").."/machine.lua")
-dofile(minetest.get_modpath("mycorners_light").."/register.lua")
---dofile(minetest.get_modpath("mycorners").."/corners.lua")
+local nodes_of_corners = {
+			{"default:clay","default_clay", "Clay", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:coalblock","default_coal_block", "Coal Block", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:cobble","default_cobble", "Cobble", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:desert_stone","default_desert_stone", "Desert Stone", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:junglewood","default_junglewood", "Jungle Wood", {choppy = 3,not_in_creative_inventory=1}},
+			{"default:pine_wood","default_pine_wood", "Pine Wood", {choppy = 3,not_in_creative_inventory=1}},
+			{"default:acacia_wood","default_acacia_wood", "Acacia Wood", {choppy = 3,not_in_creative_inventory=1}},
+			{"default:sandstone","default_sandstone", "Sandstone", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:stone","default_stone", "Stone", {cracky = 3,not_in_creative_inventory=1}},
+			{"default:wood","default_wood", "Wood", {choppy = 3,not_in_creative_inventory=1}},
+			}
+
+for i in ipairs (nodes_of_corners) do
+	local material = nodes_of_corners[i][1]
+	local image = nodes_of_corners[i][2]
+	local descr = nodes_of_corners[i][3]
+	local gro = nodes_of_corners[i][4]
+
+local dye_colors = {
+		{"dye:brown","brown"},
+		{"dye:green","green"},
+		{"dye:red","red"},
+		{"dye:white","white"},
+		}
+
+for i in ipairs (dye_colors) do
+	local dye = dye_colors[i][1]
+	local dcol = dye_colors[i][2]
+
+local cornerblock = {
+		{"wood_brown", 	       	"corners_wood_brown.png",          "Brown Wood"},
+		{"wood_green",         	"corners_wood_green.png",          "Green Wood"},
+		{"wood_red", 	       	"corners_wood_red.png",            "Red Wood"},
+		{"wood_white", 	       	"corners_wood_white.png",          "White Wood"},
+		{"stone_brown",        	"corners_stone_brown.png",         "Brown Stone"},
+		{"stone_green",      	"corners_stone_green.png",         "Green Stone"},
+		{"stone_red", 	      	"corners_stone_red.png",           "Red Stone"},
+		{"stone_white",        	"corners_stone_white.png",         "White Stone"},
+		{"stonebrick_brown", 	"corners_stonebrick_brown.png",    "Brown Stone Brick"},
+		{"stonebrick_green",  	"corners_stonebrick_green.png",    "Green Stone Brick"},
+		{"stonebrick_red", 	"corners_stonebrick_red.png",      "Red Stone Brick"},
+		{"stonebrick_white", 	"corners_stonebrick_white.png",    "White Stone Brick"},
+		}
+
+for i in ipairs(cornerblock) do
+	local mat = cornerblock[i][1]
+	local img = cornerblock[i][2]
+	local desc = cornerblock[i][3]
+
+
+local other_images = {
+		{"top","bottom"},
+		}
+for i in ipairs (other_images) do
+	local top = other_images[i][1]
+	local bottom = other_images[i][2]
+
+
+--Makes the craft item corner
+minetest.register_craftitem("mycorners_light:corner_"..mat, {
+	description = desc.." Corner",
+	inventory_image = img,
+	groups = {not_in_creative_inventory=1},
+
+})
+
+minetest.register_craft({
+    output = "mycorners_light:corner_wood_"..dcol.." 8";
+    recipe = {
+        { "default:wood", dye, "", },
+        { "", "", "", },
+        { "", "", "", },
+    }
+})
+minetest.register_craft({
+    output = "mycorners_light:corner_stone_"..dcol.." 8";
+    recipe = {
+        { "default:stone", dye, "", },
+        { "", "", "", },
+        { "", "", "", },
+    }
+})
+minetest.register_craft({
+    output = "mycorners_light:corner_stonebrick_"..dcol.." 8";
+    recipe = {
+        { "default:stonebrick", dye, "", },
+        { "", "", "", },
+        { "", "", "", },
+    }
+})
+
+--Nodes
+minetest.register_node("mycorners_light:cornerblock_"..image.."_"..mat, {
+	description = descr.."_Corner"..desc,
+	drawtype = "normal",
+	paramtype = "light",
+	tiles = {
+		image..".png^corners_"..mat.."_"..top..".png",
+		image..".png^corners_"..mat.."_"..bottom..".png",
+		image..".png",
+		image..".png^"..img.."^[transformFX",
+		image..".png",
+		image..".png^"..img,
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = gro,
+
+})
+
+dofile(minetest.get_modpath("mycorners_light").."/cornertool.lua")
+end
+end
+end
+end
+
 
 
 
